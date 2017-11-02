27fe320162fa65b34e1958602b052bcbc77541fa
Andaroth <ax.fiolle@gmail.com>
2017-11-02 21:21:07 +0100

    blink

--- led_blink/led_blink.py
+++ led_blink/led_blink.py
@@ -1,21 +1,27 @@
 import RPi.GPIO as GPIO
+import time
+
 GPIO.setmode(GPIO.BOARD)
 
 # getmode
-configuration = GPIO.getmode()
-print("conf: "+configuration)
+#configuration = GPIO.getmode()
+#print("conf: "+configuration)
 
 # config les E/S
-GPIO.setup(3, GPIO.OUT)
-GPIO.setup(9, GPIO.IN)
+GPIO.setup(3, GPIO.OUT, initial = 1)
+#GPIO.setup(9, GPIO.IN)
 
-state = GPIO.gpio_function(pin)
-print(state)
+#state = GPIO.gpio_function(pin)
+#print(state)
 
+"""
 p = GPIO.PWM(3, 1) # PWM sur 3, fréquence haute
 p.start(50)
 p.ChangeFrequency(0) # fréquence basse (off)
 p.ChangeDutCycle(50)
 p.stop
+"""
 
-GPIO.cleanup()
+while True:
+    GPIO.output(3, not GPIO.input(3))
+    time.sleep(0.5)
