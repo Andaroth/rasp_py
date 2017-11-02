bc2ce460df14b954abe90106aaafa889d6b09c87
Andaroth <ax.fiolle@gmail.com>
2017-11-02 21:02:02 +0100

    set PWM

--- led_blink/led_blink.py
+++ led_blink/led_blink.py
@@ -5,11 +5,17 @@ GPIO.setmode(GPIO.BOARD)
 configuration = GPIO.getmode()
 print("conf: "+configuration)
 
-# setting the I/O
+# config les E/S
 GPIO.setup(3, GPIO.OUT)
 GPIO.setup(9, GPIO.IN)
 
 state = GPIO.gpio_function(pin)
 print(state)
+
+p = GPIO.PWM(3, 1) # PWM sur 3, fréquence haute
+p.start(50)
+p.ChangeFrequency(0) # fréquence basse (off)
+p.ChangeDutCycle(50)
+p.stop
 
 GPIO.cleanup()
