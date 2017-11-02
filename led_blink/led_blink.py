47fd1c0cf308b1e896e72d35d8916dca656b03af
Anda <ax.fiolle@gmail.com>
2017-11-02 21:37:28 +0100

    count

--- led_blink/led_blink.py
+++ led_blink/led_blink.py
@@ -1,6 +1,8 @@
+#!/usr/bin/python
 import RPi.GPIO as gp
 import time
-gp.cleanup()
+
+count = 0
 
 # serigraphie
 gp.setmode(gp.BOARD)
@@ -10,5 +12,7 @@ gp.setup(3, gp.OUT, initial = 1)
 
 # easy blink
 while True:
+    count+=1;
+    print("Run : " + count + ". pin 3 is on " + gp.input(3));
     gp.output(3, not gp.input(3))
     time.sleep(0.5)
