1a476e9806630d786176dfbba1adeadae4ffa3d3
Anda <ax.fiolle@gmail.com>
2017-11-02 21:22:59 +0100

    blink fix

--- led_blink/led_blink.py
+++ led_blink/led_blink.py
@@ -1,27 +1,13 @@
 import RPi.GPIO as GPIO
 import time
 
+# serigraphie
 GPIO.setmode(GPIO.BOARD)
-
-# getmode
-#configuration = GPIO.getmode()
-#print("conf: "+configuration)
 
 # config les E/S
 GPIO.setup(3, GPIO.OUT, initial = 1)
-#GPIO.setup(9, GPIO.IN)
 
-#state = GPIO.gpio_function(pin)
-#print(state)
-
-"""
-p = GPIO.PWM(3, 1) # PWM sur 3, fréquence haute
-p.start(50)
-p.ChangeFrequency(0) # fréquence basse (off)
-p.ChangeDutCycle(50)
-p.stop
-"""
-
+# easy blink
 while True:
     GPIO.output(3, not GPIO.input(3))
     time.sleep(0.5)
